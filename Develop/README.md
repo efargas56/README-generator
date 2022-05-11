acf402d547974daef87e038b5c7d08f40f43491c
Eduardo Fargas <efargas56@Eduardos-MBP.attlocal.net>
2022-05-11 14:05:49 -0400

    added more utils

---
+++ Develop/README.md
@@ -0,0 +1,22 @@
+# as
+ ##Description
+  as
+  ##Table of content
+- [Installation](#installation)
+- [Usage](#usage)
+- [Tests](#tests)
+- [Credits](#credits)
+- [License](#license)
+- [Contact](#contact)
+##Installation
+as
+##Usage
+sa
+##Tests
+npm test
+##credits
+a
+##Contact
+Find me on GitHub at sa or send me an email at s
+##License
+MIT
--- Develop/index.js
+++ Develop/index.js
@@ -2,8 +2,12 @@
 const fs = require('fs');
 
 const inquirer = require('inquirer');
+const { resolve } = require('path');
 
-const generateMarkdown = require ('.utils/generateMarkdown.js')
+const generateMarkdown = require ('./utils/generateMarkdown');
+
+const renderLicenseBadge = require ('./utils/licensing');
+
 // TODO: Create an array of questions for user input
 
 const questions = () => {
@@ -110,13 +114,13 @@ const questions = () => {
         type: 'list',
         name: 'license',
         message: 'What licenses does your project use?',
-        choices: ['MIT', 'ISC', 'GNU'],
+        choices: ['MIT', 'ISC', 'GNU', 'APACHE 2.0'],
         default: ['MIT'],
         validate: nameInput => {
             if (nameInput) {
                 return true;
             } else {
-                console.log('Please enter your email address!');
+                console.log('Please choose a license!');
                 return false; 
             }
         }
@@ -125,8 +129,8 @@ const questions = () => {
     },
     {
         type: 'input',
-        name: 'contributing',
-        message: 'how can users contribute? (if applicable)'
+        name: 'contributors',
+        message: 'Who contributed to this project? (if applicable)'
     },
     {
         type: 'input',
@@ -154,33 +158,65 @@ const writeToFile = data => {
       });
     });
 }
-// if (!README.md)
-// function writeToFile(answers.title, data) {
-//     return new Promise((resolve, reject) => {
-//         fs.writeFile(fileName, data, err =>{
-            
-        
-//         if (err) {
-//             reject(err);
-//             return;
-//         }
-//         resolve({
-//             ok: true,
-//             message: 'File Created!'
-//         });
-//     });
-//     });
-// };
+const appendToFile = renderLicenseBadge => {
+    fs.appendFile('README.md', renderLicenseBadge, (err) =>{
+        if (err) {
+            reject(err);
+            return;
+        }
+        resolve({
+            ok: true,
+            message: 'file appended!'
+        });
+    });
+}
+// function renderLicenseBadge(license) {  let licenseType = `${data.license}`;
+// let licenseBadge = ''
+// if(licenseType === 'MIT') {
+//   licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`
+//   return licenseBadge;
+// } else if (licenseType === 'ISC') {
+//   licenseBadge = `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]`
+//   return licenseBadge;
+// } else if (licenseType === 'GNU') {
+//   licenseBadge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]`
+//   return licenseBadge;
+// } else if (licenseType === 'APACHE') {
+//   licenseBadge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)] `
+//   return licenseBadge;
+// }
+//  }
 
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+// function renderLicenseLink(license) { 
+//   let licenseType = `${data.license}`;
+//   let licenseUrl = ''
+//   if(licenseType === 'MIT') {
+//    licenseUrl = `(https://opensource.org/licenses/MIT)`
+//    return licenseUrl;
+//   } else if (licenseType === 'ISC') {
+//    licenseUrl = `(https://opensource.org/licenses/ISC)`
+//    return licenseUrl;
+//   } else if (licenseType === 'GNU') {
+//    licenseUrl = `(https://www.gnu.org/licenses/gpl-3.0)`
+//    return licenseUrl;
+//   } else if (licenseType === 'APACHE') {
+//    licenseUrl = `(https://opensource.org/licenses/Apache-2.0)`
+//    return licenseUrl;
+//   }
+//    }
 // TODO: Create a function to initialize app
-// function init() {}
-
-// Function call to initialize app
-// init();
-questions().then(answers => {
+ function init() { 
+     questions().then(answers => {  
     return generateMarkdown(answers);
 }).then(data => {
-    return writeToFile
+    return writeToFile(data)
+}).then(renderLicenseBadge =>{
+    return appendToFile(renderLicenseBadge)
 }).catch (err => {
     console.log(err)
 })
+ }
+// Function call to initialize app
+ init();
